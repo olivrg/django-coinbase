384d4b9bcac42c4de0230129600fea15200a5d39
Oli Guei <olivrg@gmail.com>
2022-01-11 08:54:03 +0000

    Create coinbase charge

--- core/settings.py
+++ core/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+COINBASE_COMMERCE_API_KEY = "9f803369-4cb5-400a-8dc9-a09767002f39"
--- payments/views.py
+++ payments/views.py
@@ -1,6 +1,27 @@
+# Create your views here.
+from coinbase_commerce.client import Client
 from django.shortcuts import render
 
+from core import settings
 
-# Create your views here.
+
 def home_view(request):
-    return render(request, "home.html", {})
+    client = Client(api_key=settings.COINBASE_COMMERCE_API_KEY)
+    domain_url = "http://localhost:8000/"
+    product = {
+        "name": "Coffee",
+        "description": "A really good local coffee.",
+        "local_price": {"amount": "5.00", "currency": "USD"},
+        "pricing_type": "fixed_price",
+        "redirect_url": domain_url + "success/",
+        "cancel_url": domain_url + "cancel/",
+    }
+    charge = client.charge.create(**product)
+
+    return render(
+        request,
+        "home.html",
+        {
+            "charge": charge,
+        },
+    )
